22cc4d58120634cae793328cec3d906749c4deb4
Anonymus1145 <Anonymus1145@yahoo.com>
2024-01-12 18:47:21 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/index.html","./src/**/*.{html,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
